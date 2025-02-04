a12588bdd05346047f2419a0b7ad3ef2a9b4eae9
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-02-04 22:43:34 +0300

    Connect the ruff linter

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,13 +1,13 @@
-from flask import Flask
 import os
-from dotenv import load_dotenv
 
+from dotenv import load_dotenv
+from flask import Flask
 
 load_dotenv()
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return 'Hello!'
+    return "Hello!"
