a1cd134c6bb1ef1c0f4a42d60dda88378ad916bd
Alina Makarovskaya <makarovskaya1307@gmail.com>
2025-02-25 22:47:40 +0300

    Reduce code duplication in class methods in repository.py

--- page_analyzer/repository.py
+++ page_analyzer/repository.py
@@ -17,41 +17,50 @@ class BaseRepository:
     def _connect(self):
         return psycopg2.connect(**self.connection_params)
 
+    def _execute_and_fetch_one(self, query, params=None):
+        with self._connect() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(query, params)
+                return cur.fetchone()
+
+    def _execute_and_fetch_all(self, query, params=None):
+        with self._connect() as conn:
+            with conn.cursor(cursor_factory=RealDictCursor) as cur:
+                cur.execute(query, params)
+                return cur.fetchall()
+
+    def _execute_and_fetch_id(self, query, params=None, fetch_id=False):
+        with self._connect() as conn:
+            with conn.cursor() as cur:
+                cur.execute(query, params or ())
+                if fetch_id:
+                    return cur.fetchone()[0]
+            conn.commit()
+
 
 class UrlsRepository(BaseRepository):
     def get_entities(self):
-        with self._connect() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("""SELECT * FROM urls
-                            ORDER BY id DESC, created_at DESC""")
-                return cur.fetchall()
+        return self._execute_and_fetch_all(
+            """SELECT * FROM urls ORDER BY id DESC, created_at DESC"""
+        )
 
     def get_url_data_by_id(self, id):
-        with self._connect() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls WHERE id = %s", (id,))
-                return cur.fetchone()
+        query = "SELECT * FROM urls WHERE id = %s"
+        return self._execute_and_fetch_one(query, (id,))
+
+    def get_url_data_by_name(self, url):
+        return self._execute_and_fetch_one(
+            "SELECT * FROM urls WHERE name = %s", (url,)
+        )
 
     def save_url(self, url):
         created_at = datetime.now().strftime('%Y-%m-%d')
-
-        with self._connect() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """INSERT INTO urls (name, created_at)
-                    VALUES (%s, %s) RETURNING id""",
-                    (url["url"], created_at)
-                )
-                url_id = cur.fetchone()[0]
-            conn.commit()
-
-        return url_id
-
-    def get_url_data_by_name(self, url):
-        with self._connect() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("SELECT * FROM urls WHERE name = %s", (url,))
-                return cur.fetchone()
+        return self._execute_and_fetch_id(
+            """INSERT INTO urls (name, created_at)
+               VALUES (%s, %s) RETURNING id""",
+            (url["url"], created_at),
+            fetch_id=True
+        )
 
 
 class UrlChecksRepository(BaseRepository):
@@ -66,38 +75,23 @@ class UrlChecksRepository(BaseRepository):
         ) AS c ON u.id = c.url_id
         ORDER BY u.id DESC;
         """
-        with self._connect() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute(query)
-                return cur.fetchall()
+        return self._execute_and_fetch_all(query)
 
     def get_checks_by_id(self, url_id):
-        with self._connect() as conn:
-            with conn.cursor(cursor_factory=RealDictCursor) as cur:
-                cur.execute("""
-                            SELECT * FROM url_checks
-                            WHERE url_id = %s
-                            ORDER BY id DESC, created_at DESC
-                            """,
-                            (url_id,)
-                            )
-                return cur.fetchall()
+        return self._execute_and_fetch_all(
+            """SELECT * FROM url_checks
+                WHERE url_id = %s
+                ORDER BY id DESC, created_at DESC""",
+            (url_id,)
+        )
 
     def save_url_check(self, id, status_code, h1, title, description):
         created_at = datetime.now().strftime('%Y-%m-%d')
-
-        with self._connect() as conn:
-            with conn.cursor() as cur:
-                cur.execute(
-                    """
-                    INSERT INTO url_checks (url_id, status_code, h1,
-                    title, description, created_at)
-                    VALUES (%s, %s, %s, %s, %s, %s)
-                    RETURNING id
-                    """,
-                    (id, status_code, h1, title, description, created_at)
-                )
-                url_check_id = cur.fetchone()[0]
-            conn.commit()
-
-        return url_check_id
+        return self._execute_and_fetch_id(
+            """INSERT INTO url_checks (url_id, status_code, h1,
+               title, description, created_at)
+               VALUES (%s, %s, %s, %s, %s, %s)
+               RETURNING id""",
+            [id, status_code, h1, title, description, created_at],
+            fetch_id=True
+        )
